c598f60410ba22e02d30a01ac4173f97dc6a1118
Kenny Boateng <kennyboateng.99@gmail.com>
2024-09-13 12:03:39 +0200

    Initial commit

---
+++ src/main/java/com/example/GestioneTravels/GestioneTravelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestioneTravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneTravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneTravelsApplication.class, args);
+	}
+
+}
